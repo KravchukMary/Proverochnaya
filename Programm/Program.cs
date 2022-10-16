4f77a4a249d97a82debe8256f45719902157d22f
Mariya <kravchuk_mary@mail.ru>
2022-10-16 21:19:59 +0300

    Создание второго заголовка

--- Programm/Program.cs
+++ Programm/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Задача: Написать программу, которая из сформированного массива строк содержащих символы 
 // и числа создать массив из строк, в который попадут только символы, не являющиеця цифрами.
 
-char[] collection = ("Зада4ча: Напи3сать програм23му, котор4ая").ToCharArray();
+char[] stroki = ("За03дача5: На8пи3сать про6грам3му, ко4тор4ая").ToCharArray();
 var result = string.Empty;
-foreach(var item in collection)
+foreach(var item in stroki)
 {
 if(!char.IsDigit(item))
 result+= item;
