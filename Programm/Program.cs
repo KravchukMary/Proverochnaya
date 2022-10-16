68dba4efee90bbd4633d666135343724fa2a91d8
Mariya <kravchuk_mary@mail.ru>
2022-10-16 21:12:36 +0300

    Создали массив char

--- Programm/Program.cs
+++ Programm/Program.cs
@@ -1,2 +1,11 @@
 ﻿// Задача: Написать программу, которая из сформированного массива строк содержащих символы 
 // и числа создать массив из строк, в который попадут только символы, не являющиеця цифрами.
+
+char[] collection = ("Зада4ча: Напи3сать програм23му, котор4ая").ToCharArray();
+var result = string.Empty;
+foreach(var item in collection)
+{
+if(!char.IsDigit(item))
+result+= item;
+}
+Console.WriteLine(result);
